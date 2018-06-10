1e71c3b463c20dde7c7302b64b4de0d98e314910
meytala <meytala@users.noreply.github.com>
2018-06-10 21:59:39 +0300

    june 10 2018

--- See_nopsis.py
+++ See_nopsis.py
@@ -5,12 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from decimal import Decimal
 from numpy import percentile
-
-
-# from scipy import stats
-# from scipy.stats import zscore
-# import matplotlib.pyplot as plt
-
+from pandas.api.types import is_string_dtype
+from io import StringIO
 
 
 #######################################################
@@ -26,7 +22,6 @@ from numpy import percentile
 ###in the beta version, only CSV will be allowed as an input table from the working directory
 ###the function will take the name that the user wrote and add to it ".csv"
 
-
 def name_of_table (name):
     str1 = str(name)
     str2 = ".csv"
@@ -36,7 +31,8 @@ def name_of_table (name):
 
 
 def table_as_df (table_name):
-    df = pd.read_csv(table_name, parse_dates=True,infer_datetime_format=True)
+    global df
+    df = pd.read_csv(table_name)
     #print("this is the database: ", df)                                       ###QA
     return df
 
@@ -77,6 +73,8 @@ def name_of_variables (df):
 
 
 
+
+
 ######################################################################
 ################count the number of variables in a dataset############
 ######################################################################
@@ -91,29 +89,47 @@ def count_var (df):
 
 
 
-########################################################################
-####################create a list of Variable Type######################
-########################################################################
 
-""" a function that detect the type of the variable from the original table and add it in a new column in the var_table"""
 
-def var_types (df):
-    var_type_list = []
-    for var in df:
-        #var_type = df[var].dtype
-        #var_type= type(var)
-        var_type = np.dtype(df[var])
-        var_type_list.append(var_type)
-    #print ("list of variables type:", var_type_list)
-    return var_type_list
 
+######################################################################
+################identifying the type of the variable #################
+######################################################################
+
+#####identify the type of the variable
+##potential types:
+
+###if dtype is in (int64, float64 - than numeric (unless only 2 unique)
+####if dtype is object
+
+#bollian - only 2 unique values (except for null)
+#categorical - < 10 uniqe variables - of numeric - no need if string - there is a need
+#continuoues - >10 unique variables
+
+
+
+# if list_of_binary_variable = []
+# for i in name_of_variables(table_as_df(name_of_table ("test_data"))):
+#     if df[i].nunique()== 2:
+#         list_of_binary_variable.append (i)
+# print("the binary variables are:", list_of_binary_variable)
+
+# list_of_categorical_variable = []
+# for i in name_of_variables(table_as_df(name_of_table ("test_data"))):
+#     if df[i].nunique() >=3 and df[i].nunique() <= 10:
+#         list_of_categorical_variable.append (i)
+# print("the category variables are:",  list_of_categorical_variable)
+
+
+
+# df.name.unique()
 
 
 
 
 ##################calling tHe functions#############3
 
-table_name = name_of_table ("weather")
+table_name = name_of_table ("test_data")
 print("The name of the table is: ", table_name)
 
 df_table = table_as_df (table_name)
@@ -125,13 +141,12 @@ print("The number of records in the dataset is: ", record_count)
 column_name_list = name_of_variables (df_table)
 print("This is a list with the names of the variables: ", column_name_list)
 
+#
+# if is_string_dtype(column_name_list[4]==True):
+#     print ("the variable {} is a string".format ("name3"))
+
 number_of_variables = count_var (df_table)
 print("In this dataset, the number of variables is: ", number_of_variables)
-
-variable_type_list = var_types (df_table)
-print("This is a list with the type of the variables: ",variable_type_list)
-
-
 
 
 #######################################################
@@ -143,7 +158,8 @@ print("This is a list with the type of the variables: ",variable_type_list)
 ### if there is more than one row with the same ID - function will only work for first row
 ### will be identified if each row has a unique variable
 
-
+##input: df
+##output: the index of the coloumn in which the ID is present
 
 ##NEED TO CODE
 
@@ -152,71 +168,83 @@ print("This is a list with the type of the variables: ",variable_type_list)
 ####################Basic Statistic######################
 #########################################################
 
-"""a function that gives you basic statistics"""
+"""Instances of the Class represents the variable"""
+####decided to run as a class, than, for each variable (will turn to object) there will be methods
+####the output of the method will be displayed in the output table
 
-class Variable:
-    def __init__(self, name, values, type, index):
+
+class ContVariable:
+    def __init__(self, name, values, index):
         self.name = name
         self.values=values
-        self.type = type
         self.index = index
+
+    def var_type (self):
+        type_of_variable = np.dtype(df[self.name])
+        return type_of_variable
 
     def mean_of_var (self):
         name = self.name
         mean = np.mean(self.values)
         #print("the mean of the coloum {} is {}".format (name, mean))
-        return round(mean,2)
+        return round(mean,2)    ####this function returns mean round to 2 decimal
 
     def median_of_var (self):
         name = self.name
         median = np.nanmedian(self.values)
         #print("the median of the coloum {} is {}".format (name, median))
-        return round(median,1)
+        return round(median,1)   ####this function returns median round to 2 decimal
 
     def lower_iqr (self):
         name = self.name
         values = self.values
         low_iqr = np.nanpercentile(values,25)
-        return low_iqr
+        return low_iqr       ####this function returns the lower boundry of IQR
 
     def upper_iqr(self):
         name = self.name
         values = self.values
-        up_iqr = np.nanpercentile(values, 75)
+        up_iqr = np.nanpercentile(values, 75)    ####this function returns the upper boundry of IQR
         return up_iqr
 
     def minimum_of_var (self):
         name = self.name
         minimum = np.min(self.values)
-        #print("the minimal value of coloum {} is {}".format (name, minimum))
-        return minimum
+        #print("the minimal value of coloum {} is {}".format (name, minimum))    ##QA
+        return minimum ####this function returns the minimal value of the variable
 
     def maximum_of_var (self):
         name = self.name
         maximum = np.max(self.values)
-        #print("the maximal value of coloum {} is {}".format (name, maximum))
-        return maximum
+        #print("the maximal value of coloum {} is {}".format (name, maximum))    ##QA
+        return maximum    ####this function returns the maximal value of the variable
 
     def sd_of_var (self):
         name = self.name
         sd = np.std(self.values)
-        #print("the std of coloum {} is {}".format (name, sd))
-        return round(sd,2)
+        #print("the std of coloum {} is {}".format (name, sd))        ##QA
+        return round(sd,2)   #####this function returns the sd, round to 2 decimals
 
     def count_null (self):
         name = self.name
         values = self.values
         number_of_null=values.isnull().sum()
-        return number_of_null
+        return number_of_null   ####this function returns the number of nulls in the variable
 
+    def number_of_outliers(self, outlier_constant):
+        a = np.array(self.values)
+        upper_quartile = np.nanpercentile(a,75)
+        lower_quartile = np.nanpercentile(a, 25)
+        IQR = (upper_quartile - lower_quartile)
+        extend_IQR = IQR * outlier_constant
+        louer_boundry = lower_quartile - extend_IQR
+        upper_boundry = upper_quartile + extend_IQR
+        count=0
+        for y in a:
+            if (y <= louer_boundry) or (y >= upper_boundry):
+                count +=1
+        return count   ####this function returns the number of outliers, based on bounderies +/-  X times IQR
 
-    # def outliers (self):
-    #     name = self.name
-    #     threshold = 2
-    #     outlier = df[name[(np.abs(stats.zscore()) > threshold)]]
-    #     return ("there number of outliers of the variable {} is {}".format (name, outlier))
-
-    #
     def histogram (self):
         plt.hist(self.values.dropna(), bins=50)    ######this returns n=array, bins, patch=Silent list of individual patches used to create the histogram
         plt.savefig("hist_{}".format(self.index))
@@ -224,82 +252,50 @@ class Variable:
         return self.index
 
 
-    ###QA
-# name_2 = column_name_list[1]                            #QA
+    def pie (self):
+        self.values.value_counts().plot(kind='pie')
+        # plt.pie(self.values.dropna())    ######this returns n=array, bins, patch=Silent list of individual patches used to create the histogram
+        plt.savefig("pie_{}".format(self.index))
+        plt.close()
+        counts = self.values.value_counts()
+        return self.index
+
+
+    def bars (self):
+        self.values.value_counts().plot(kind='barh')
+        # plt.pie(self.values.dropna())    ######this returns n=array, bins, patch=Silent list of individual patches used to create the histogram
+        plt.savefig("bars_{}".format(self.index))
+        plt.close()
+        counts = self.values.value_counts()
+        return self.index
+
+# #     ###QA
+# name_2 = column_name_list[9]                            #QA
 # values_2 = df_table[name_2]
-# type_2 = variable_type_list[1]
-# test = Variable(name_2, values_2, type_2, index=1)
-# print("histogram", test.histogram())
-# # # , test.median_of_var(), test.minimum_of_var(), test.maximum_of_var(), test.sd_of_var())
-
-
-
-#######################################################
-####################Variable histogrial################
-#######################################################
-
-""" a function that build an histogram"""
-
-
+# test = ContVariable(name_2, values_2, index=9)
+# # print("var_type", test.var_type()) ###there is a problem, date returns as an object
+# # # # , test.median_of_var(), test.minimum_of_var(), test.maximum_of_var(), test.sd_of_var())
+# print("pie",test.pie() )
 
 
 #######################################################
-####################Missing Value######################
+#######################OBJECTS#########################
 #######################################################
-
-"""a function that is looking for the missing values"""
-
-
-# name_1 = name_of_variables (df)[1]                            #QA
-# values_1 = df[name_of_variables (df)[1]].values
-# test = Variable(name_1, values_1 )
-# print(test.maximum_of_var())
-# # # , test.median_of_var(), test.minimum_of_var(), test.maximum_of_var(), test.sd_of_var())
-
-
-
 
 ######creating a list of objects for the Variable class. Each object has to have:
-#  1. a name (the name of the variable)   - line 67, def: name_of_variables, line 119 - the name of the list (column_name_list)
-#  2. The values of the variables  - in the dataframe - the values of rows 1 and more (0 is the title) - line 33 the def, line 113 - name of table: df_table
-#  3. The type of the variable - line 94: var type, line 125: name of variable: variable_type_list
+#  1. a name - the name of the variable
+#  2. The values of the variable
+#  3. The index of the variable
 
-## I have extracted all of these properties before (lines
 
 list_of_objects = []
 for index, variable in enumerate (column_name_list):
     name = variable
     values = df_table[name]  ##in pandas - this is how you get the values
-    type = variable_type_list[index]
     index=index
-    list_of_objects.append(Variable(name, values, type,index))
+    list_of_objects.append(ContVariable(name, values, index))
 
 #print("list of objects: ", list_of_objects)            #QA
-
-
-##### for each object, apply the methods of the class Variable. The stat methods are only for variables that are int64 and float64
-##### store it as a list
-
-# stat_list = []
-# for object in list_of_objects:
-#     if object.type.name in ('int64', 'float64'):
-#         stat_list.append ( [object.name,
-#                       object.type,
-#                       object.mean_of_var(),
-#                       object.sd_of_var(),
-#                       object.median_of_var(),
-#                       object.lower_iqr(),
-#                       object.upper_iqr(),
-#                       object.minimum_of_var(),
-#                       object.maximum_of_var(),
-#                       object.count_null(),
-#                       object.histogram()])   ###appending the list with few variables
-#
-#
-# ##print ("this is the stat list: ", stat_list)            ##QA
-
-
-
 
 
 #######################################################
@@ -308,54 +304,93 @@ for index, variable in enumerate (column_name_list):
 
 """ a function that wrap everything nicely in a table to display """
 
-##https://stackoverflow.com/questions/1475123/easiest-way-to-turn-a-list-into-an-html-table-in-python
-
-######first, given a "flat list", produce a list of sublists
-
-html_top = """<html>
-<table border = 5 border-spacing: 0.5rem>
-<caption>SEENOPSIS</caption>
-    <tr>
-        <th>Variable name</th>
-        <th>Type</th>
-        <th>Mean &plusmn sd</th>
-        <th>Median (IQR) </th>
-        <th>Min</th>
-        <th>Max</th>
-        <th>Null</th>
+html_top = """<html> 
+<table border = 5 border-spacing: 0.1rem> 
+<caption>SEENOPSIS</caption> 
+<caption>This is the seenopsis of the detaset:  {}</caption> 
+<caption>This detaset has {} records and {} variables</caption> 
+    <tr> 
+        <th>Variable name</th>  
+        <th>Type</th> 
         <th>Histogram</th>
-    </tr>"""
-
-#<th>histogram</th>
-
-html_bottomn = """</table>
-</html>"""
+        <th>Basic Stat</th>
+        <th>Null</th> 
+        <th>Outliers (n)</th>  
+    </tr>""".format(table_name, record_count, number_of_variables  )
 
 
 body_list = []
 for object in list_of_objects:
-    if object.type.name in ('int64', 'float64'):
-        list_for_body = "<tr>" \
-                            "<th> {} </ th>" \
-                            "<td> {} </ td>" \
-                            "<td> {} &plusmn  <br> {} </ td>" \
-                            "<td> {} <br> ({}, {}) </ td>" \
-                            "<td> {} </ td>" \
-                            "<td> {} </ td>" \
-                            "<td> {} </ td>" \
-                            "<td> <img src='hist_{}.png' width ='200' hight='150'> </img> </td>"\
-                         "</tr>".format ( object.name,
-                                       object.type,
-                                          object.mean_of_var(),
-                                          object.sd_of_var(),
-                                          object.median_of_var(),
-                                          object.lower_iqr(),
-                                          object.upper_iqr(),
-                                          object.minimum_of_var(),
-                                          object.maximum_of_var(),
-                                          object.count_null(),
-                                          object.histogram())
+    if object.var_type() in ('int64', 'float64'):
+        if object.values.nunique()<= 2:
+            list_for_body = """<tr>
+                            <th> {} </ th>
+                            <td> Binaric Variable </ td>
+                            <td> <img src='pie_{}.png' width='200' hight='200'> </img> </td>
+                            <td> Binary variable </ td>
+                            <td> {} </ td>
+                            <td> Binaric variable - No outliers </ td>
+                         </tr>""".format (object.name,
+                                          object.pie(),
+                                          object.count_null())
+            body_list.append(list_for_body)
+        else: list_for_body = """<tr>
+                            <th> {} </ th>
+                            <td> {} </ td>
+                            <td> <img src='hist_{}.png' width ='200' hight='150'> </img> </td>
+                            <td> Min: {} 
+                            <br> Max: {} 
+                            <br> Mean &plusmn SD: {} &plusmn {} 
+                            <br> Median (IQR): {} ({}, {}) </ td>
+                            <td> {} </ td>
+                            <td> {} </ td>
+                         </tr>""".format ( object.name,
+                                        object.var_type(),
+                                        object.histogram(),
+                                        object.minimum_of_var(),
+                                        object.maximum_of_var(),
+                                        object.mean_of_var(),
+                                        object.sd_of_var(),
+                                        object.median_of_var(),
+                                        object.lower_iqr(),
+                                        object.upper_iqr(),
+                                        object.count_null(),
+                                        object.number_of_outliers(outlier_constant=1.5))
         body_list.append(list_for_body)
+    elif object.var_type() == 'object':
+         if object.values.nunique()<= 2:
+            list_for_body = """<tr>
+                            <th> {} </ th>
+                            <td> Binaric Variable </ td>
+                            <td> <img src='bars_{}.png' width ='200' hight='150'> </img> </td>
+                            <td> Binaric variable  
+                            <td> {} </ td>
+                            <td> Binaric variable - No outliers </ td>
+                         </tr>""".format (object.name,
+                                          object.bars(),
+                                          object.count_null())
+            body_list.append(list_for_body)
+         else: list_for_body = """<tr>
+                            <th> {} </ th>
+                            <td> Categorial Variable </ td>
+                            <td> <img src='hist_{}.png' width ='200' hight='150'> </img> </td>
+                            <td> Categorial variable  
+                            <td> {} </ td>
+                            <td> Categorial variable - No outliers </ td>
+                         </tr>""".format (object.name,
+                                          object.histogram(),
+                                          object.count_null())
+         body_list.append(list_for_body)
+
+
+
+
+
+
+
+html_bottomn = """</table>
+</html>"""
+
 
 
 ##############################the histogram doesn't show. in the function (219) I return a saved file. I tried show.
@@ -364,8 +399,7 @@ merged_html = html_top + "".join(body_list) +html_bottomn
 
 ##print(merged_html)            ##QA
 
-
-with open("test2.html","w") as html_file:
+with open("output_seenopsis.html","w") as html_file:
     html_file.write(merged_html)
     html_file.close()
 
