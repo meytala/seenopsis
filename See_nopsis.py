52a5e054df825299dc7c01bfb50f99919b7b9601
meytala <meytala@users.noreply.github.com>
2018-06-11 21:36:56 +0300

    June 11 update

--- See_nopsis.py
+++ See_nopsis.py
@@ -7,6 +7,10 @@ from decimal import Decimal
 from numpy import percentile
 from pandas.api.types import is_string_dtype
 from io import StringIO
+import matplotlib.cm as cm
+
+
+######find dates and the distribution in % of binary variable
 
 
 #######################################################
@@ -32,7 +36,7 @@ def name_of_table (name):
 
 def table_as_df (table_name):
     global df
-    df = pd.read_csv(table_name)
+    df = pd.read_csv(table_name, parse_dates=True, date_parser=pd.datetools.to_datetime)
     #print("this is the database: ", df)                                       ###QA
     return df
 
@@ -95,6 +99,35 @@ def count_var (df):
 ######################################################################
 ################identifying the type of the variable #################
 ######################################################################
+
+#####identify the type of the variable
+##potential types:
+
+###if dtype is in (int64, float64 - than numeric (unless only 2 unique)
+####if dtype is object
+
+#bollian - only 2 unique values (except for null)
+#categorical - < 10 uniqe variables - of numeric - no need if string - there is a need
+#continuoues - >10 unique variables
+
+
+
+# if list_of_binary_variable = []
+# for i in name_of_variables(table_as_df(name_of_table ("test_data"))):
+#     if df[i].nunique()== 2:
+#         list_of_binary_variable.append (i)
+# print("the binary variables are:", list_of_binary_variable)
+
+# list_of_categorical_variable = []
+# for i in name_of_variables(table_as_df(name_of_table ("test_data"))):
+#     if df[i].nunique() >=3 and df[i].nunique() <= 10:
+#         list_of_categorical_variable.append (i)
+# print("the category variables are:",  list_of_categorical_variable)
+
+
+
+# df.name.unique()
+
 
 
 
@@ -233,7 +266,7 @@ class ContVariable:
 
 
     def bars (self):
-        self.values.value_counts().plot(kind='barh')
+        self.values.value_counts()[:10].plot(kind='barh')
         # plt.pie(self.values.dropna())    ######this returns n=array, bins, patch=Silent list of individual patches used to create the histogram
         plt.savefig("bars_{}".format(self.index))
         plt.close()
@@ -283,7 +316,7 @@ html_top = """<html>
     <tr> 
         <th>Variable name</th>  
         <th>Type</th> 
-        <th>Histogram</th>
+        <th>Graphic Representation</th>
         <th>Basic Stat</th>
         <th>Null</th> 
         <th>Outliers (n)</th>  
@@ -297,16 +330,30 @@ for object in list_of_objects:
             list_for_body = """<tr>
                             <th> {} </ th>
                             <td> Binaric Variable </ td>
-                            <td> <img src='pie_{}.png' width='200' hight='200'> </img> </td>
+                            <td> <img src='bars_{}.png' width='200' hight='200'> </img> </td>
                             <td> Binary variable </ td>
                             <td> {} </ td>
                             <td> Binaric variable - No outliers </ td>
                          </tr>""".format (object.name,
-                                          object.pie(),
+                                          object.bars(),
                                           object.count_null())
+            body_list.append(list_for_body)
+        elif object.values.nunique()>2 and object.values.nunique()<6 :
+            list_for_body = """<tr>
+                            <th> {} </ th>
+                            <td> Categoric Variable </ td>
+                            <td> <img src='bars_{}.png' width='200' hight='200'> </img> </td>
+                            <td> Categoric Variable </ td>
+                            <td> {} </ td>
+                            <td> Categoric Variable - No outliers </ td>
+                         </tr>""".format (object.name,
+                                          object.bars(),
+                                          object.count_null())
+            body_list.append(list_for_body)
         else: list_for_body = """<tr>
                             <th> {} </ th>
-                            <td> {} </ td>
+                            <td> Continuous variable 
+                            <br>({}) </ td>
                             <td> <img src='hist_{}.png' width ='200' hight='150'> </img> </td>
                             <td> Min: {} 
                             <br> Max: {} 
@@ -326,8 +373,9 @@ for object in list_of_objects:
                                         object.upper_iqr(),
                                         object.count_null(),
                                         object.number_of_outliers(outlier_constant=1.5))
+        body_list.append(list_for_body)
     elif object.var_type() == 'object':
-        if object.values.nunique()<= 2:
+        if object.values.nunique()<= 2 and object.values.nunique()>0:
             list_for_body = """<tr>
                             <th> {} </ th>
                             <td> Binaric Variable </ td>
@@ -338,8 +386,9 @@ for object in list_of_objects:
                          </tr>""".format (object.name,
                                           object.bars(),
                                           object.count_null())
-        elif object.values.nunique()> 2 and object.values.nunique() <=20:
-             list_for_body = """<tr>
+            body_list.append(list_for_body)
+        elif object.values.nunique()> 2 and object.values.nunique()<=20:
+            list_for_body = """<tr>
                             <th> {} </ th>
                             <td> Categorial Variable </ td>
                             <td> <img src='bars_{}.png' width ='200' hight='150'> </img> </td>
@@ -349,25 +398,21 @@ for object in list_of_objects:
                          </tr>""".format (object.name,
                                           object.bars(),
                                           object.count_null())
+            body_list.append(list_for_body)
+
         else:
-             list_for_body = """<tr>
-                            <th> {} </ th>
-                            <td> Categorial Variable </ td>
-                            <td> <img src='hist_{}.png' width ='200' hight='150'> </img> </td>
-                            <td> Categorial variable  
-                            <td> {} </ td>
-                            <td> Categorial variable - No outliers </ td>
-                         </tr>""".format (object.name,
-                                          object.histogram(),
-                                          object.count_null())
-    body_list.append(list_for_body)
-
-
-
-
-
-
-
+            list_for_body = """<tr>
+                                <th> {} </ th>
+                                <td> Text variable</ td>
+                                <td> <img src='bars_{}.png' width ='200' hight='150'> </img> </td>
+                                <td> Text variable - 
+                                <br> only top 10 values are presented </td>
+                                <td> {} </ td>
+                                <td> Text variable - No outliers </ td>
+                             </tr>""".format(object.name,
+                                             object.bars(),
+                                             object.count_null())
+            body_list.append(list_for_body)
 
 html_bottomn = """</table>
 </html>"""
@@ -383,4 +428,3 @@ merged_html = html_top + "".join(body_list) +html_bottomn
 with open("output_seenopsis.html","w") as html_file:
     html_file.write(merged_html)
     html_file.close()
-
