00bf1b3394049ce924ddc0d2bd22d3c9285923c5
meytala <meytala@users.noreply.github.com>
2018-07-13 17:20:23 +0300

    July 13 update
    cleaned code, added unittest

--- seenopsis.py
+++ seenopsis.py
@@ -1,3 +1,7 @@
+from typing import Any, Union
+
+from numpy.core.multiarray import ndarray
+
 __author__ = "Meytal Avgil Tsadok"
 __copyright__ = "Copyright 2018, TLV Israel"
 __credits__ = "She codes - Final project"
@@ -54,19 +58,17 @@ def process_pandas_df(name_of_pandas_df):                                       
 
 
 def convert_csv_to_pd(csv_file_name):                                                 ###a function that take csv in spesific encodings and transfer it to df
-    df = None
+    local_df = None
     for encoding in ['utf-8', 'UTF-8', 'ANSI', 'ISO-8859-1', 'ISO-8859-8']:
         try:
-            df = pd.read_csv(csv_file_name, parse_dates=True, infer_datetime_format=True, date_parser=pd.to_datetime, encoding=encoding)
+            local_df = pd.read_csv(csv_file_name, parse_dates=True, infer_datetime_format=True, date_parser=pd.to_datetime, encoding=encoding)
             print("The csv file is encoded with {}".format(encoding))
             break
         except UnicodeDecodeError:
             print("The csv file is not encoded with {}".format(encoding))
-        except pd.errors.DtypeWarning:
-            print("At least one of your column has mixed type")
         except pd.errors.ParserError:
             print("Please use a csv file for your database")
-    return df
+    return local_df
 
 
 #########################################################
@@ -96,10 +98,32 @@ class VariableInfo:
         self.index = index
 
     def var_type(self):                                                             ### a variable that get the variable type based on numpy
-        type_of_variable = np.dtype(df[self.name])
+        type_of_variable = np.dtype(self.values)
         return type_of_variable
 
-    def type(self):                                                                 ### a function that subdivid the types for more accurate categories
+    def type_for_operation(self):
+        if self.var_type() in ('int64', 'float64', 'int32', 'float32'):
+            if self.values.nunique() == 1:
+                return "single number"
+            elif self.values.nunique() == 2:
+                return "binary number"
+            elif 2 < self.values.nunique() <= 10:
+                return "category number"
+            else:
+                return "continuous number"
+        elif self.var_type() == 'object':
+            if self.values.nunique() == 1:
+                return "single text"
+            elif self.values.nunique() == 2:
+                return "binary text"
+            elif 2 < self.values.nunique() <= 10:
+                return "category text"
+            else:
+                return "general text"
+        else:
+            return "general text"
+
+    def type(self):                                                                ### a function that subdivid the types for more accurate categories
         if self.var_type() in ('int64', 'float64', 'int32', 'float32'):
             if self.values.nunique() == 1:
                 return "Single Variable\n" \
@@ -107,7 +131,7 @@ class VariableInfo:
             elif self.values.nunique() == 2:
                 return "Binary Variable\n" + \
                        " ({})".format(self.var_type())
-            elif self.values.nunique() > 2 and self.values.nunique() <= 10:
+            elif 2 < self.values.nunique() <= 10:
                 return "Categorical variable\n" + \
                        " ({})".format(self.var_type())
             else:
@@ -125,49 +149,48 @@ class VariableInfo:
         else:
             return "Text or Date"
 
-    def histogram(self):                                                            ### a function that create a histogram with 50 bins/20%!!!
-        # if self.values.nunique()>50:
-        #     bins_num=int(self.values.nunique()*0.3)
-        # else:
-        #     bins_num=20
+    def histogram(self):                                                     ### a function that create a histogram with 50 bins/20%!!!
         plt.hist(self.values.dropna(), bins=50)
+        plt.yticks(fontsize=14)
+        plt.xticks(fontsize=14)
         plt.savefig(graph_dir + "hist_{}".format(self.index))
         plt.close()
         return "hist_{}.png".format(self.index)
 
     def bars(self):                                                                 ### a function that create an horisontal barchart
         self.values.value_counts().nlargest(10).plot(kind='barh')
+        plt.yticks(fontsize=14)
+        plt.xticks(fontsize=14)
         plt.savefig(graph_dir + "bars_{}".format(self.index))
         plt.close()
         return "bars_{}.png".format(self.index)
 
     def graph(self):                                                                ### a function that match the graph type to present based on the variable type
-        if self.var_type() in ('int64', 'float64', 'int32', 'float32') and self.values.nunique() > 10:
+        my_type = self.type_for_operation()
+        if my_type == "continuous number":
             return self.histogram()
-        elif self.values.nunique() > 0:
-            return self.bars()
         else:
-            return "No graphic representation"
+            return self.bars()
 
     def mean_of_var(self):                                                          ### this function returns mean round to 2 decimal
         name = self.name
         mean = np.mean(self.values)
-        # print("the mean of the coloum {} is {}".format (name, mean))
+        # print("the mean of the column {} is {}".format (name, mean))
         return round(float(mean), 2)
 
     def median_of_var(self):                                                        ### this function returns median round to 2 decimal
         name = self.name
         median = np.nanmedian(self.values)
-        # print("the median of the coloum {} is {}".format (name, median))
+        # print("the median of the column {} is {}".format (name, median))
         return round(float(median), 2)
 
-    def lower_iqr(self):                                                            ### this function returns the lower boundry of IQR
+    def lower_iqr(self):                                                            ### this function returns the lower boundary of IQR
         name = self.name
         values = self.values
         low_iqr = np.nanpercentile(values, 25)
         return round(low_iqr, 2)
 
-    def upper_iqr(self):                                                            ### this function returns the upper boundry of IQR
+    def upper_iqr(self):                                                            ### this function returns the upper boundary of IQR
         name = self.name
         values = self.values
         up_iqr = np.nanpercentile(values, 75)
@@ -176,39 +199,39 @@ class VariableInfo:
     def minimum_of_var(self):                                                       ### this function returns the minimal value of the variable
         name = self.name
         minimum = np.min(self.values)
-        # print("the minimal value of coloum {} is {}".format (name, minimum))    ##QA
+        # print("the minimal value of column {} is {}".format (name, minimum))    ##QA
         return round(minimum, 2)
 
     def maximum_of_var(self):                                                       ### this function returns the maximal value of the variable
         name = self.name
         maximum = np.max(self.values)
-        # print("the maximal value of coloum {} is {}".format (name, maximum))    ##QA
+        # print("the maximal value of column {} is {}".format (name, maximum))    ##QA
         return round(maximum, 2)
 
     def sd_of_var(self):                                                            ### this function returns the sd, round to 2 decimals
         name = self.name
         sd = np.std(self.values)
-        # print("the std of coloum {} is {}".format (name, sd))        ##QA
+        # print("the std of column {} is {}".format (name, sd))        ##QA
         return round(float(sd), 2)
 
-    def statistics(self):                                                                  ### this function returns what will be written in the statistic coloumn - based on variable type
-        if self.var_type() in ('int64', 'float64', 'int32', 'float32') and self.values.nunique() > 10:
+    def statistics(self):                                                                  ### this function returns what will be written in the statistic column - based on variable type
+        if self.type_for_operation() == "continuous number":
             return ["Min: {}".format(self.minimum_of_var()),
                     "Max: {}".format(self.maximum_of_var()),
                     "Mean &plusmn SD: {} &plusmn {}".format(self.mean_of_var(), self.sd_of_var()),
                     "Median (IQR): {} ({}, {})".format(self.median_of_var(), self.lower_iqr(), self.upper_iqr())]
 
-        elif 2 < self.values.nunique() <= 10:
+        elif "category" in self.type_for_operation():
             return ["Categorical Variable",
                     "{} unique values".format(self.unique_categories()),
                     "Up to top 10 values are presented"]
 
-        elif self.values.nunique() == 2:
+        elif "binary" in self.type_for_operation():
             return ["Binary variable",
                     "{}: {}%".format(self.count_binary()[0][0], self.count_binary()[0][1]),
                     "{}: {}%".format(self.count_binary()[1][0], self.count_binary()[1][1])]
 
-        elif self.values.nunique() == 1:
+        elif "unique" in self.type_for_operation():
             return ["Single value",
                     "No Statistics"]
 
@@ -229,26 +252,26 @@ class VariableInfo:
             return [("N={}, {}%".format(number_of_null, percent_of_null))]
 
     def number_of_outliers(self, outlier_constant):                                       ### this function counts the number of outliers based on a distance of XX IQR
-        if self.var_type() in ('int64', 'float64', 'int32', 'float32') and self.values.nunique() > 10:
+        if self.type_for_operation() == "continuous number":
             a = np.array(self.values)
             upper_quartile = np.nanpercentile(a, 75)
             lower_quartile = np.nanpercentile(a, 25)
             IQR = (upper_quartile - lower_quartile)
             extend_IQR = IQR * outlier_constant
-            lower_boundry = lower_quartile - extend_IQR
-            upper_boundry = upper_quartile + extend_IQR
+            lower_boundary: Union[Union[int, float, complex, ndarray], Any] = lower_quartile - extend_IQR
+            upper_boundary = upper_quartile + extend_IQR
             count = 0
             for y in a:
-                if (y <= lower_boundry) or (y >= upper_boundry):
+                if (y <= lower_boundary) or (y >= upper_boundary):
                     count += 1
             return ["N={}".format(count)]
-        elif 2 < self.values.nunique() < 10:
+        elif "category" in self.type_for_operation():
             return ["Categorical variable",
                     "No outlier"]
-        elif self.values.nunique() == 2:
+        elif "binary" in self.type_for_operation():
             return ["Binary variable",
                     "No outlier"]
-        elif self.values.nunique() == 1:
+        elif "single" in self.type_for_operation():
             return ["Single variable",
                     "No outlier"]
         else:
@@ -277,7 +300,6 @@ class VariableInfo:
 #  1. a name - the name of the variable
 #  2. The values of the variable
 #  3. The index of the variable"""
-
 
 def list_of_object(column_name_list, df_table):                                                       ###this functin create a list of objects with the attributes of VariableInfo
     list_of_objects = []
@@ -334,12 +356,12 @@ def build_html():
             <table class="table table-hover" style="margin-bottom:0px">
                 <thead>
                 <tr align="right" >
-                    <th width="7%">Variable<br/>Name</th>
-                      <th width="15%" >Type</th>
-                      <th width="15%">Graphic<br/> Representation</th>
-                      <th width="15%">Basic<br/> Statistics</th>
-                      <th width="15%">Missing</th>
-                      <th width="15%">Outliers(n)</th>
+                    <th width="10%">Variable<br/>Name</th>
+                      <th width="10%" >Type</th>
+                      <th width="10%">Graphic<br/> Representation</th>
+                      <th width="10%">Basic<br/> Statistics</th>
+                      <th width="10%">Missing</th>
+                      <th width="10%">Outliers (n)</th>
                 </tr></thead></table>
              </div>
           </div>
@@ -351,12 +373,12 @@ def build_html():
              <table class="content-table">
                 <thead>
                 <tr >
-                   <th width="7%"></th>
-                   <th width="15%"></th>
-                   <th width="15%"></th>
-                   <th width="15%"></th>
-                   <th width="15%"></th>
-                   <th width="15%"></th>
+                   <th width="10%"></th>
+                   <th width="10%"></th>
+                   <th width="10%"></th>
+                   <th width="10%"></th>
+                   <th width="10%"></th>
+                   <th width="10%"></th>
                 </tr>
                 </thead>""".format(number_of_variables, record_count)
 
@@ -365,9 +387,9 @@ def build_html():
         list_for_body = """ 
         <tr align="left" class="single-row">
         <th width="10%"  align="left"> {} </th>
-        <td width="15%" align="left"> {} </td>
-        <td width="20%" align="left"> <img src='Graphs_for_seenopsis/{}' width='200' hight='200'> </img> </td>
-        <td width="15%" align="left"> {} </td>
+        <td width="10%" align="left"> {} </td>
+        <td width="10%" align="left"> <img src='Graphs_for_seenopsis/{}' width='200' hight='200'> </img> </td>
+        <td width="10%" align="left"> {} </td>
         <td width="10%" align="left"> {} </td>
         <td width="10%" align="left"> {} </td>
         <tr align="left" class="single-row">""".format \
@@ -425,5 +447,3 @@ def build_html():
 ####call seenopsis from a different tab
 # import seenopsis
 # seenopsis.process_csv()
-
-
